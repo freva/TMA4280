f35d4078af01167143aa1213ae50291e089ab5b1
freva <valerijf@stud.ntnu.no>
2015-02-15 16:36:53 +0100

    Added OpenMP implementation

---
+++ Oving4/openmp.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "../common/common.h"
+
+
+void vecSum(int N) {
+    double vectorSum = 0, pi = 4.0*atan(1.0);
+    Vector vector = createVector(N);
+
+    #pragma omp parallel for schedule(static) reduction(+:vectorSum)
+    for (long int i=1; i<=N; i++) {
+        double temp=1.0/(i*i);
+        vector->data[i] = temp;
+        vectorSum += temp;
+    }
+
+
+    printf("N = %d | Vector sum: %f\n", N, vectorSum);
+    printf("Difference: %1.16f\n\n", pi*pi/6.0 - vectorSum);
+}
+
+
+int main() {
+    double startTime = WallTime();
+    for(int i=8; i<=16384; i*=2) {
+        vecSum(i);
+    }
+	
+    printf("Finished in %1.2f\n", WallTime() - startTime);
+}
